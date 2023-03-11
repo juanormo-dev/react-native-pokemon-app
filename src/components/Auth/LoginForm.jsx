c2d7fbf183d94d7104d6f637fc24089d5ac2a59e
juanormo <juanormo@outlook.com>
2023-03-11 12:30:19 -0500

    feat: 🎸 Clase 03 - Formulario de login

--- src/components/Auth/LoginForm.jsx
+++ src/components/Auth/LoginForm.jsx
@@ -1,12 +1,51 @@
 import React from "react";
-import { Text, View } from "react-native";
+import {
+  Button,
+  Keyboard,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 const LoginForm = () => {
   return (
     <View>
-      <Text>LoginForm</Text>
+      <Text style={styles.title}>Iniciar Sesión</Text>
+      <TextInput
+        placeholder="Usuario"
+        style={styles.input}
+        autoCapitalize="none"
+      />
+      <TextInput
+        placeholder="Contraseña"
+        style={styles.input}
+        secureTextEntry={true}
+        autoCapitalize="none"
+      />
+      <Button
+        title="Ingresar"
+        onPress={() => {
+          console.log("Ingresando...");
+        }}
+      />
     </View>
   );
 };
-
+const styles = StyleSheet.create({
+  title: {
+    textAlign: "center",
+    fontSize: 28,
+    fontWeight: "bold",
+    marginTop: 50,
+    marginBottom: 15,
+  },
+  input: {
+    height: 50,
+    margin: 12,
+    borderWidth: 1,
+    padding: 15,
+    borderRadius: 10,
+  },
+});
 export default LoginForm;
